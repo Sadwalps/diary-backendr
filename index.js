54b668ed2987a04b4427b4d0cfd75e70ed55fafe
Sadwalps <sadwalps2255@gmail.com>
2026-09-24 21:13:20 +0530

    model, controller folders created and router.js and index.js files modified

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ require(`dotenv`).config()
 //import express
 const express = require(`express`)
 
-//create server
-const diaryServer = express()
+//import cors
+const cors = require('cors')
+
+//import router
+const router = require('./router')
 
 //import connection
 require('./connection')
+
+//create server
+const diaryServer = express()
+
+//tell the server to use cors
+diaryServer.use(cors())
+
+//tell the server to use express.json
+diaryServer.use(express.json())
+
+//tell the server to use router
+diaryServer.use(router)
 
 //set port for the server
 const PORT = 4001 || process.env.PORT
 
 //make the server listen to that port
-diaryServer.listen(PORT,()=>{
+diaryServer.listen(PORT, () => {
     console.log(`Server running successfully at port : ${PORT}`);
+})
+
+//to check the requset
+diaryServer.get(`/`, (req, res) => {
+    res.send(`Get request recieved`)
 })
